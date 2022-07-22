f419691c3cec6d2d4d313a29212017a521d15128
gabriel-olv <br.gabrieldeoliveira@gmail.com>
2022-07-22 20:09:17 -0300

    Implementando classe de serviço crud para cliente

---
+++ backend/src/main/java/br/com/gabrieldeoliveira/msr/domain/services/ServicoCrudCliente.java
@@ -0,0 +1,52 @@
+package br.com.gabrieldeoliveira.msr.domain.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.gabrieldeoliveira.msr.domain.model.Cliente;
+import br.com.gabrieldeoliveira.msr.domain.repositories.RepositorioCliente;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class ServicoCrudCliente {
+
+    private RepositorioCliente repositorioCliente;
+
+    @Transactional
+    public Cliente salvar(Cliente novo) {
+        return repositorioCliente.save(novo);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cliente> listarTodos() {
+        return repositorioCliente.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Cliente buscarPorId(Long id) {
+        return repositorioCliente.findById(id).orElseThrow(() -> new RuntimeException());
+    }
+
+    @Transactional
+    public Cliente atualizar(Long clienteId, Cliente dadosNovos) {
+        Cliente cliente = buscarPorId(clienteId);
+
+        //Atualiza-se nome, email e telefone se vier os dados.
+        final String nome = dadosNovos.getNome();
+        final String email = dadosNovos.getEmail();
+        final String telefone = dadosNovos.getTelefone();
+        if (nome != null || nome != "") {
+            cliente.setNome(nome);
+        }
+        if (email != null || email != "") {
+            cliente.setEmail(email);
+        }
+        if (telefone != null || telefone != "") {
+            cliente.setTelefone(telefone);
+        }
+        return cliente;
+    }
+}
